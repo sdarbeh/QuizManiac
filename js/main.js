e89eb677aff4416d2edf7be44b14ac4c4a508e81
Sam Darbeh <darbeh.samuel99@gmail.com>
2020-01-19 23:11:21 -0600

    V1

--- js/main.js
+++ js/main.js
@@ -1,14 +1,36 @@
-let fondsAmount = document.querySelectorAll(".coins-amount");
-let url;
-let argent = 0;
-let gagneArgent = 0;
-let totalArgent = gagneArgent + argent;
 let correct = 0;
 let missed = 0;
 let index = 0;
+let gameMode;
+const hundredScoreMessage = [
+    "Congratulations and BRAVO!",
+    "This calls for celebrating! Congratulations!",
+    "You did it! So proud of you!",
+    "I knew it was only a matter of time. Well done!"
+];
+const midScoreMessage = [
+    "Good job, you're so close to a 100%",
+    "So close, keep it up!",
+    "Wow! Good job",
+    "Awesome! Can you get a 100% next time? I know you can!"
+];
+const failScoreMessage = [
+    "Better luck next time!",
+    "Keep your head up champ!",
+    "You got this! Try again?",
+    "Oh no, looks like the game level was too hard?"
+];
 
-$(".mode").click(function () {
-  goToCategories();
+$(".mode").click(function (e) {
+    goToCategories();
+    if (e.target.textContent === "Easy") {
+        gameMode = "easy"
+    } else if (e.target.textContent === "Medium") {
+        gameMode = "medium"
+    } else {
+        gameMode = "hard"
+    }
+
 });
 
 $(".return").click(function () {
@@ -21,15 +43,17 @@ function startGame() {
     setStats();
     for (let i = 0; i < category.length; i++) {
         category[i].addEventListener("click", () => {
-            // alert(category[i].classList);
+
             goToGame();
-            let url = `https://opentdb.com/api.php?amount=10&category=${category[i].classList}&difficulty=easy&type=multiple`;
+
+            let url = `https://opentdb.com/api.php?amount=10&category=${category[i].classList}&difficulty=${gameMode}&type=multiple`;
 
             fetch(url)
                 .then(checkStatus)
                 .then(response => response.json())
                 .then(data => {
                     generateQuiz(data.results);
+                    viewAnswers(data.results);
                 })
                 .catch(error => {
                     $(".question").html("Oops! An error occurred. Please refresh and try again.");
@@ -48,21 +72,20 @@ function startGame() {
 } startGame();
 
 function generateQuiz(data) {
-
-    $(".game-mode-display").html(`Difficulty: <span>${data[index].difficulty}</span>`);
+    if (gameMode === "hard") {
+        gameMode = "M A N I A C"
+    }
+    $(".game-mode-display").html(`Difficulty: <span>${gameMode}</span>`);
     $(".game-category").html(`${data[index].category}`);
     $(".current-question").html(index + 1 +"/10");
     $(".question").html(`${data[index].question}`);
-
 
     $(".choice1").html(`${data[index].incorrect_answers[0]}`);
     $(".choice2").html(`${data[index].incorrect_answers[1]}`);
     $(".choice3").html(`${data[index].incorrect_answers[2]}`);
     $(".choice4").html(`${data[index].correct_answer}`);
 
-
     $(".choice").shuffle();
-
     checkAnswer(data);
 }
 
@@ -73,6 +96,7 @@ function checkAnswer(data) {
         choice[i].addEventListener("click", () => {
 
             if (choice[i].innerHTML === data[index].correct_answer) {
+
                 if (index < 9) {
                     guessRight();
                     generateQuiz(data);
@@ -81,7 +105,9 @@ function checkAnswer(data) {
                     goToResults();
                     setResultsStats(data);
                 }
+
             } else {
+
                 if (index < 9) {
                     guessWrong();
                     generateQuiz(data);
@@ -96,24 +122,13 @@ function checkAnswer(data) {
 }
 
 function setStats() {
-    let correctAmount = document.querySelector(".correct-amount");
-    let wrongAmount = document.querySelector(".incorrect-amount");
-
-    correctAmount.textContent = correct.toString();
-    wrongAmount.textContent = missed.toString();
-    for (let i = 0; i < fondsAmount.length; i++) {
-        if (localStorage.getItem("localArgent") === null) {
-            fondsAmount[i].textContent = numberWithCommas(totalArgent).toString() + "$";
-        } else {
-            fondsAmount[i].textContent = (localStorage.getItem("localArgent")) + "$";
-        }
-    }
+    $(".correct-amount").html(correct.toString());
+    $(".incorrect-amount").html(missed.toString());
 }
+
 function guessRight() {
     index++;
     correct++;
-    argent++;
-    gagneArgent++;
     setStats();
 }
 function guessWrong() {
@@ -124,15 +139,28 @@ function guessWrong() {
 
 function setResultsStats(data) {
     let difficultyLevel = document.querySelector(".results-level");
+    let gameCategory = document.querySelector(".result-game-category");
     let scored = document.querySelector(".you-scored");
     let scoredPercent = document.querySelector(".score-percentage");
     let percentage = Math.floor((correct / 10) * 100);
-    let coinsGained = document.querySelector(".coins-gained");
+    let message = document.querySelector(".message");
+    let randomNumber = Math.round(Math.random() * 4);
 
     difficultyLevel.innerHTML = `Difficulty: ${data[0].difficulty}`;
+    gameCategory.innerHTML = `Category <br><span>${data[0].category}</span>`;
     scored.innerHTML = "Score: " + "<span>" + correct.toString() + "/10</span>";
     scoredPercent.innerHTML = percentage.toString() + "%";
-    coinsGained.innerHTML = "Coins gained: <span>" + gagneArgent.toString() + "</span>"
+
+    if (percentage === 100) {
+        message.innerHTML = hundredScoreMessage[randomNumber];
+        scoredPercent.style.color = "green"
+    } else if (percentage <= 90 && percentage > 60) {
+        message.innerHTML = midScoreMessage[randomNumber];
+        scoredPercent.style.color = "green"
+    } else {
+        message.innerHTML = failScoreMessage[randomNumber];
+        scoredPercent.style.color = "red"
+    }
 }
 
 function goToCategories() {
@@ -153,30 +181,12 @@ function goToResults() {
     hideContent("game-page", "category-head");
     showContent("results-page");
     whereToGo();
-    if (supportsLocalStorage()) {
-        localStorage.setItem("localArgent", JSON.stringify(totalArgent));
-    }
 }
 
-function showAnswers() {
-
-}
 function whereToGo() {
-    let options = document.querySelectorAll(".result-option");
-    for (let i = 0; i < options.length; i++) {
-        options[i].addEventListener("click", function () {
-            if (options[0]) {
-                location.reload();
-            } else if (options[1]) {
-                hideContent("results-page");
-                goToCategories();
-            } else if (options[2]) {
-                hideContent("results-page");
-                index = 0;
-                startGame();
-            }
-        })
-    }
+    $(".result-option").on("click", function () {
+        location.reload();
+    })
 }
 
 function hideContent() {
@@ -191,6 +201,34 @@ function showContent() {
         for(let i=0; i < arguments.length; i++){
             document.getElementById(arguments[i]).style.display = "block";
         }
+    }
+}
+
+function openAndCloseViewAnswers() {
+    $(".view-answers").on("click", function () {
+        $(".results").css("display", "unset")
+    });
+
+    $(".close-modal").on("click", function () {
+        $(".results").css("display", "none")
+    })
+} openAndCloseViewAnswers();
+
+function viewAnswers(data) {
+    let results = document.querySelector(".result-content");
+    for (let i = 0; i < data.length; i++) {
+
+        results.innerHTML += `
+        <div class="review-question">
+                        <h4>${data[i].question}</h4>
+                        <h5>Correct answer:</h5>
+                        <p>${data[i].correct_answer}</p>
+                        <h5>Incorrect answer(s):</h5>
+                        <p>${data[i].incorrect_answers[0]}</p>
+                        <p>${data[i].incorrect_answers[1]}</p>
+                        <p>${data[i].incorrect_answers[2]}</p>
+                    </div>
+            `
     }
 }
 
@@ -212,15 +250,3 @@ function showContent() {
         return $(shuffled);
     };
 })(jQuery);
-
-function numberWithCommas(x) {
-    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
-}
-
-function supportsLocalStorage() {
-    try {
-        return "localStorage" in window && window["localStorage"] !== null;
-    } catch (e) {
-        return false;
-    }
-}
